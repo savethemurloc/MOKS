f7cf6a6f041fb16a467ec3235004286a2af3ec43
savethemurloc <bygoga1996@gmail.com>
2022-12-26 14:20:24 +0700

    added read *.pcap file and test actions with each packet

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from scapy.all import *
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+packets = rdpcap('/home/alexandr/dump.pcap')
+print(len(packets))
+# for packet in packets:
+#     print(packet)
+#     print(dir(packet))
+#     print(packet.payload)
+#     print(packet['TCP'])
+#     print(packet['TCP'].flags)
+#
